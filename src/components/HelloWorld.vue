b3078db361a8b6929cc39f41f7c291dc02baad0e
yusuke-ten <net.escape.com@gmail.com>
2020-02-15 02:37:21 +0900

    vue class style component

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -11,9 +11,7 @@
       </v-col>
 
       <v-col class="mb-4">
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
+        <h1 class="display-2 font-weight-bold mb-3">Welcome to Vuetify</h1>
 
         <p class="subheading font-weight-regular">
           For help and collaboration with other Vuetify developers,
@@ -25,9 +23,7 @@
       </v-col>
 
       <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          What's next?
-        </h2>
+        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
 
         <v-row justify="center">
           <a
@@ -36,16 +32,13 @@
             :href="next.href"
             class="subheading mx-3"
             target="_blank"
+            >{{ next.text }}</a
           >
-            {{ next.text }}
-          </a>
         </v-row>
       </v-col>
 
       <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Important Links
-        </h2>
+        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
 
         <v-row justify="center">
           <a
@@ -54,16 +47,13 @@
             :href="link.href"
             class="subheading mx-3"
             target="_blank"
+            >{{ link.text }}</a
           >
-            {{ link.text }}
-          </a>
         </v-row>
       </v-col>
 
       <v-col class="mb-5" cols="12">
-        <h2 class="headline font-weight-bold mb-3">
-          Ecosystem
-        </h2>
+        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
 
         <v-row justify="center">
           <a
@@ -72,9 +62,8 @@
             :href="eco.href"
             class="subheading mx-3"
             target="_blank"
+            >{{ eco.text }}</a
           >
-            {{ eco.text }}
-          </a>
         </v-row>
       </v-col>
     </v-row>
@@ -82,62 +71,58 @@
 </template>
 
 <script lang="ts">
-import Vue from 'vue'
+import { Vue } from 'vue-property-decorator'
 
-export default Vue.extend({
-  name: 'HelloWorld',
-
-  data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader'
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify'
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify'
-      }
-    ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com'
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com'
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify'
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs'
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify'
-      }
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer'
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/layout/pre-defined'
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
-      }
-    ]
-  })
-})
+export default class HelloVue extends Vue {
+  ecosystem = [
+    {
+      text: 'vuetify-loader',
+      href: 'https://github.com/vuetifyjs/vuetify-loader'
+    },
+    {
+      text: 'github',
+      href: 'https://github.com/vuetifyjs/vuetify'
+    },
+    {
+      text: 'awesome-vuetify',
+      href: 'https://github.com/vuetifyjs/awesome-vuetify'
+    }
+  ]
+  importantLinks = [
+    {
+      text: 'Documentation',
+      href: 'https://vuetifyjs.com'
+    },
+    {
+      text: 'Chat',
+      href: 'https://community.vuetifyjs.com'
+    },
+    {
+      text: 'Made with Vuetify',
+      href: 'https://madewithvuejs.com/vuetify'
+    },
+    {
+      text: 'Twitter',
+      href: 'https://twitter.com/vuetifyjs'
+    },
+    {
+      text: 'Articles',
+      href: 'https://medium.com/vuetify'
+    }
+  ]
+  whatsNext = [
+    {
+      text: 'Explore components',
+      href: 'https://vuetifyjs.com/components/api-explorer'
+    },
+    {
+      text: 'Select a layout',
+      href: 'https://vuetifyjs.com/layout/pre-defined'
+    },
+    {
+      text: 'Frequently Asked Questions',
+      href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions'
+    }
+  ]
+}
 </script>
