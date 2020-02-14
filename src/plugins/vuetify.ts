1eb740dac1c3f60bea1fa4435d26fe073244fa1c
yusuke-ten <net.escape.com@gmail.com>
2020-02-15 00:38:15 +0900

    a la carte off

--- src/plugins/vuetify.ts
+++ src/plugins/vuetify.ts
@@ -1,5 +1,8 @@
 import Vue from 'vue'
-import Vuetify from 'vuetify/lib'
+// tree shaking ビルドサイズを小さくしてくれる
+// import Vuetify from 'vuetify/lib'
+// 全部インポートビルドは早い
+import Vuetify from 'vuetify'
 
 Vue.use(Vuetify)
 
