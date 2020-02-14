ff6768dcb375599dd90e0587fe8933ff0ad930e1
yusuke-ten <net.escape.com@gmail.com>
2020-02-15 01:50:28 +0900

    change configs

--- jest.config.js
+++ jest.config.js
@@ -10,17 +10,20 @@ module.exports = {
   ],
   transform: {
     // process *.vue files with vue-jest
-    '^.+\\.vue$': require.resolve('vue-jest'),
-    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$': require.resolve(
-      'jest-transform-stub'
-    ),
-    '^.+\\.jsx?$': require.resolve('babel-jest'),
-    '^.+\\.tsx?$': require.resolve('ts-jest')
+    '^.+\\.vue$': 'vue-jest',
+    '.+\\.(css|styl|less|sass|scss|svg|png|jpg|ttf|woff|woff2)$':
+      'jest-transform-stub',
+    '^.+\\.jsx?$': 'babel-jest',
+    '^.+\\.tsx?$': 'ts-jest'
   },
-  transformIgnorePatterns: ['/node_modules/'],
+  transformIgnorePatterns: ['/node_modules/(?!vuetify).+\\.js$'],
   // support the same @ -> src alias mapping in source code
   moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1'
+    '^@/(.*)$': '<rootDir>/src/$1',
+    '^~/(.*)$': '<rootDir>/src/$1',
+    '^vue$': 'vue/dist/vue.common.js',
+    '.+\\.(css|styl|less|sass|scss|png|jpg|ttf|woff|woff2)$':
+      'identity-obj-proxy'
   },
   testEnvironment: 'jest-environment-jsdom-fifteen',
   // serializer for snapshots
@@ -29,7 +32,13 @@ module.exports = {
   // https://github.com/facebook/jest/issues/6766
   testURL: 'http://localhost/',
   watchPlugins: [
-    require.resolve('jest-watch-typeahead/filename'),
-    require.resolve('jest-watch-typeahead/testname')
-  ]
+    'jest-watch-typeahead/filename',
+    'jest-watch-typeahead/testname'
+  ],
+  globalSetup: '<rootDir>/tests/unit/setup.js',
+  globals: {
+    'ts-jest': {
+      tsConfig: 'tsconfig.json'
+    }
+  }
 }
